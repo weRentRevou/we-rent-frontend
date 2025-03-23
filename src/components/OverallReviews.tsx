8e012c9a1b7cf8e59f06bbc3a1c8d14819b59287
Erlangga <erradzan@gmail.com>
2025-03-23 11:36:21 +0700

    update

--- src/components/OverallReviews.tsx
+++ src/components/OverallReviews.tsx
@@ -6,11 +6,11 @@ export default function OverallReviews() {
         <div className="bg-white max-w-sm mx-auto">
             <div>
                 <div className="flex justify-between items-center mb-2">
-                <h3 className="text-md font-medium">REVIEWS (7)</h3>
-                <a href="#" className="text-green-600 text-sm">View More &gt;</a>
+                    <h3 className="text-md font-medium">REVIEWS (7)</h3>
+                    <a href="#" className="text-green-600 text-sm">View More &gt;</a>
                 </div>
                 <div className="flex items-center space-x-1">
-                <Stars rating={4} size="text-lg" className="text-yellow" gap="gap-1" />
+                    <Stars rating={4} size="text-lg" className="text-yellow" gap="gap-1" />
                 </div>
                 <div className="mt-2 space-y-2">
                     <div>
