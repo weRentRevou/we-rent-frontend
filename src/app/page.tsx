4e78821a9353a925f4e2cdce1a09eb3fcdad7f6b
rizkyano <ridhwandinaano@gmail.com>
2025-03-23 13:21:38 +0700

    add product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,15 @@ import HorizontalDivider from "@/_components/dividers/HorizontalDivider";
 import RatingModal from "@/_components/modals/RatingModal";
 import ReviewItem from "@/_components/review/ReviewItem";
 import { useState } from "react";
+import Product from "@/_container/product";
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="min-h-screen p-5">
-      {/* Example calling the modal and ReviewItems component */}
-      {/* <HorizontalDivider className="mb-4" height="h-0.5" />
+      <Product />
+      {/* Example calling the modal and ReviewItems component
+      <HorizontalDivider className="mb-4" height="h-0.5" />
       <ReviewItem
         review="This black kaftan is a wardrobe staple for me now! The quality is outstanding, and it's incredibly versatile. I've worn it to brunch with friends, to the beach as a cover-up, and even to a formal dinner with the right accessories. It's so comfortable and easy to style. I can't recommend it enough! ps. btw im using my"
         rating={5}
@@ -22,7 +24,6 @@ export default function Home() {
         id={1}
         likes={5}
       />
-
       <button onClick={() => setIsOpen(true)}>Open Modal</button>
       <RatingModal open={isOpen} handleOpen={() => setIsOpen(!isOpen)} /> */}
     </div>
