5279c61fc8c7e0199fd54b85bb17869e33f0f28e
rizkyano <ridhwandinaano@gmail.com>
2025-03-24 12:18:02 +0700

    update weight price button

--- src/_components/button/button.tsx
+++ src/_components/button/button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function button() {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white shadow-md p-5 pb-6 flex justify-between items-center">
+    <div className="fixed bottom-0 left-0 right-0 mx-auto max-w-[400px] bg-white shadow-md p-4 pb-6 flex justify-between items-center ">
       <div>
         <p className="text-gray-500 text-sm">Rent Fee</p>
         <h3 className="text-black font-bold">Rp 300.000/4 Day</h3>
