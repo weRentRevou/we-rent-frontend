a92b4b85725f18a2931702a2c536226b30f6a059
Cahya Juniar <juniarcahya@gmail.com>
2025-03-26 21:50:51 +0700

    merge productDetail branch and adjust some parts

--- src/_containers/detail-page/OverallReviews.tsx
+++ src/_containers/detail-page/OverallReviews.tsx
@@ -1,18 +1,26 @@
 import ProgressBar from "@/_components/bars/ProgressBar";
 import HorizontalDivider from "@/_components/dividers/HorizontalDivider";
 import Stars from "@/_components/stars/Stars";
+import { Review } from "@/types/types";
 import Link from "next/link";
 
 interface ProductReviewData {
-  average_rating : number;
-  fitscale : {small : number, true_to_size : number, large : number};
+  average_rating: number;
+  fit_scale: { small: number; true_to_size: number; large: number };
+  reviews: Review[];
 }
 
-export default function OverallReviews({ reviewData }: { reviewData: ProductReviewData }) {
+export default function OverallReviews({
+  reviewData,
+}: {
+  reviewData: ProductReviewData;
+}) {
   return (
     <div className="mt-4">
       <div className="flex justify-between items-center mb-4">
-        <h3 className="text-sm font-bold">REVIEWS (7)</h3>
+        <h3 className="text-sm font-bold">
+          REVIEWS ({reviewData.reviews.length})
+        </h3>
         <Link
           href="/1/reviews"
           className="text-accent text-sm font-semibold underline"
@@ -22,9 +30,12 @@ export default function OverallReviews({ reviewData }: { reviewData: ProductRevi
       </div>
       <Stars rating={reviewData.average_rating} size="text-sm" gap="gap-1.5" />
       <div className="mt-4 flex flex-col gap-2 w-full">
-        <ProgressBar label="Small" value={reviewData.fitscale.small} />
-        <ProgressBar label="True To Size" value={reviewData.fitscale.true_to_size} />
-        <ProgressBar label="Large" value={reviewData.fitscale.large} />
+        <ProgressBar label="Small" value={reviewData.fit_scale.small} />
+        <ProgressBar
+          label="True To Size"
+          value={reviewData.fit_scale.true_to_size}
+        />
+        <ProgressBar label="Large" value={reviewData.fit_scale.large} />
       </div>
       <div className="px-5">
         <HorizontalDivider className="mt-4" />
--- src/_containers/detail-page/SizeGuide.tsx
+++ src/_containers/detail-page/SizeGuide.tsx
@@ -10,28 +10,30 @@ interface Product {
 export default function SizeGuide({ productData }: { productData: Product }) {
   return (
     <Suspense fallback={<div className="p-4 text-center">Loading...</div>}>
-    <div className="mt-4">
-      <div>
-        <h3 className="text-xs font-bold mb-2">SIZE GUIDE</h3>
-        <table className="w-full border-collapse border border-light-gray">
-          <thead>
-            <tr className="border-b border-b-light-gray text-xs">
-              <th className="w-1/3 p-2">SIZE</th>
-              <th className="w-1/3 p-2">BUST</th>
-              <th className="w-1/3 p-2 ">LENGTH</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr className="text-xs font-light">
-              <td className="p-2 text-center">{productData.size}</td>
-              <td className="p-2 text-center">{productData.bust} cm</td>
-              <td className="p-2 text-center">{productData.length} cm</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="mt-4">
+        <div>
+          <h3 className="text-xs font-bold mb-2">SIZE GUIDE</h3>
+          <table className="w-full border-collapse border border-light-gray">
+            <thead>
+              <tr className="border-b border-b-light-gray text-xs">
+                <th className="w-1/3 p-2">SIZE</th>
+                <th className="w-1/3 p-2">BUST</th>
+                <th className="w-1/3 p-2 ">LENGTH</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="text-xs font-light">
+                <td className="p-2 text-center uppercase">
+                  {productData.size}
+                </td>
+                <td className="p-2 text-center">{productData.bust} cm</td>
+                <td className="p-2 text-center">{productData.length} cm</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <HorizontalDivider className="mt-6" height="h-0.5" />
       </div>
-      <HorizontalDivider className="mt-6" height="h-0.5" />
-    </div>
     </Suspense>
   );
 }
